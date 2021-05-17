a4bddaa782595587ae3fcb4d4f80f25b2f45096c
Kennedy Ribeiro <kennedy2000silva@hotmail.com>
2021-05-16 21:31:10 -0300

    add: dist to gitignore

--- gw-api/dist/Modules/Categories/categories.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Category {
-    id: number;
-    name: string;
-    description: string;
-    games: number;
-    popularity: number;
-}
--- gw-api/dist/Modules/Categories/categories.providers.d.ts
+++
@@ -1,7 +0,0 @@
-import { Connection, Repository } from 'typeorm';
-import { Category } from './categories.entity';
-export declare const CategoryProviders: {
-    provide: string;
-    useFactory: (connection: Connection) => Repository<Category>;
-    inject: string[];
-}[];
